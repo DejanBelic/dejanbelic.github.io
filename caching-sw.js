aeffb395c4aaf3ddbefe7225d420a3715e32f775
Dejan <dejan.belic@develomon.com>
2018-11-30 15:57:59 +0100

    changed approach

--- caching-sw.js
+++ caching-sw.js
@@ -1,10 +1,12 @@
 
-navigator.serviceWorker.register('/OneSignalSDKWorker.js')
 
-console.log('bbb')
-// self.addEventListener('activate', function () {
-//     console.log('[Service worker] activated.') 
-// });
+    OneSignal.init({
+        appId: 'aff45c2b-b8cc-4b01-8bed-e2eb8c203dd6'
+    })
+
+    navigator.serviceWorker.register(
+        "https://dejanbelic.github.io/caching-sw.js", { scope: "/" } //root scope for the whole site
+    );
 
     window.addEventListener('load', function () {
         console.log('daaa')
@@ -16,3 +18,4 @@ console.log('bbb')
             console.log('ServiceWorker registration failed: ', err);
         });
     });
+
