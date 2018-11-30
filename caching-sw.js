a5e7c5da21a1d544bc9b0da77f96d13570ea335d
Dejan <dejan.belic@develomon.com>
2018-11-30 15:29:31 +0100

    diff approach

--- caching-sw.js
+++ caching-sw.js
@@ -1,10 +1,20 @@
-navigator.serviceWorker.register('/OneSignalSDKWorker.js');
 
 
-self.addEventListener('install', function () {
-    console.log('[Service Worker] installed.') 
-});
 
-self.addEventListener('activate', function () {
-    console.log('[Service worker] activated.') 
-});
+
+// self.addEventListener('activate', function () {
+//     console.log('[Service worker] activated.') 
+// });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        console.log('daaa')
+        navigator.serviceWorker.register('/OneSignalSDKWorker.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
