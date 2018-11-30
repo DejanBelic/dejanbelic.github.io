23544c76e3e0ec24bb66ff95508fa6cd166cb29e
Dejan <dejan.belic@develomon.com>
2018-11-30 16:03:39 +0100

    Commented sw of one signal and add custom sw.

--- caching-sw.js
+++ caching-sw.js
@@ -1,16 +1,16 @@
 
 
-    OneSignal.init({
-        appId: 'aff45c2b-b8cc-4b01-8bed-e2eb8c203dd6'
-    })
+    // OneSignal.init({
+    //     appId: 'aff45c2b-b8cc-4b01-8bed-e2eb8c203dd6'
+    // })
 
-    navigator.serviceWorker.register(
-        "https://dejanbelic.github.io/caching-sw.js", { scope: "/" } //root scope for the whole site
-    );
+    // navigator.serviceWorker.register(
+    //     "https://dejanbelic.github.io/caching-sw.js", { scope: "/" } //root scope for the whole site
+    // );
 
+if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        console.log('daaa')
-        navigator.serviceWorker.register('/OneSignalSDKWorker.js').then(function (registration) {
+        navigator.serviceWorker.register('/sw.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
@@ -18,4 +18,4 @@
             console.log('ServiceWorker registration failed: ', err);
         });
     });
-
+}
