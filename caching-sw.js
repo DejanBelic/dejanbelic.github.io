88c7af0b6a2718c9da1204f6aac02bdf9b1d4694
Dejan <dejan.belic@develomon.com>
2018-11-30 16:25:31 +0100

    add app.js to register sw.

--- caching-sw.js
+++ caching-sw.js
@@ -1,21 +1,7 @@
+self.addEventListener('install', function (event) {
+    console.log('[Service worker installed]', event)
+})
 
-
-    // OneSignal.init({
-    //     appId: 'aff45c2b-b8cc-4b01-8bed-e2eb8c203dd6'
-    // })
-
-    // navigator.serviceWorker.register(
-    //     "https://dejanbelic.github.io/caching-sw.js", { scope: "/" } //root scope for the whole site
-    // );
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+self.addEventListener('activate', function (event) {
+    console.log('[Service worker activation', event)
+})
