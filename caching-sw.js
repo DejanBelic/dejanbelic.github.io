3722a178004554affdc1b1f2d5de7d0f1cda3b8e
Dejan <dejan.belic@develomon.com>
2018-12-03 09:03:06 +0100

    Added folder structure and static files for caching, cached static files.

--- caching-sw.js
+++ caching-sw.js
@@ -1,6 +1,22 @@
+var CACHE_STATIC_NAME = 'static-v01';
+var STATIC_FILES = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    'css/main.css',
+    'images/mici.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:300,400'
+] 
+
+
 self.addEventListener('install', function (event) {
     console.log('[Service worker installed]', event)
-    event.
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME).then(function (cache) {
+            console.log('[Service Worker] Precaching App Shell...', event);
+            cache.addAll(STATIC_FILES);
+        })
+    )
 })
 
 self.addEventListener('activate', function (event) {
