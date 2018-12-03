01485ebaa68d8afc6f42e2b918585cbe97f1e83d
Dejan <dejan.belic@develomon.com>
2018-12-03 09:32:10 +0100

    Added fallback page and added dynamic caching.

--- caching-sw.js
+++ caching-sw.js
@@ -1,7 +1,9 @@
 var CACHE_STATIC_NAME = 'static-v01';
+var CACHE_DYNAMIC_NAME = 'dynamic_cache-v01'
 var STATIC_FILES = [
     '/',
     '/index.html',
+    '/offline.html',
     '/js/app.js',
     'css/main.css',
     'images/mici.jpg',
@@ -28,8 +30,25 @@ self.addEventListener('fetch', function (event) {
         caches.match(event.request)
          .then(function (response) {
              if (response) {
+                 console.log(response)
                  return response;
              }
+             else {
+                 return fetch(event.request)
+                  .then(function (respo) {
+                      return caches(CACHE_DYNAMIC_NAME)
+                       .then(function (cache) {
+                           return respo;
+                       })
+                  })
+             }
+         })
+         .catch(function () {
+             // Return fallback page.
+             return caches.open(CACHE_STATIC_NAME)
+              .then(function (cache) {
+                  return cache.match('/offline.html') // Command for getting something from cache.
+              })
          })
     )
 });
